a369ff460966fcc5540780b501af51e335fb3ced
whicheverpeady <whicheverpeady@gmail.com>
2015-08-30 19:56:34 -0400

    Add jquery for dropdown navigation

---
+++ app/assets/javascripts/dropdown.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('#navbar li').hover(
+    function () {
+        //show submenu
+        $('ul', this).show();
+    }, function () {
+        //hide submenu
+        $('ul', this).hide();
+    });
+});
