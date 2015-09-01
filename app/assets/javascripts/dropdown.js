827b135c04032cfccdeed641b4d0ed77e4227a19
whicheverpeady <whicheverpeady@gmail.com>
2015-08-31 22:35:24 -0400

    Restructure header lists for dropdown capability

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
-    $('#navbar li').hover(
-    function () {
-        //show submenu
-        $('ul', this).show();
-    }, function () {
-        //hide submenu
-        $('ul', this).hide();
-    });
-});
+    $('#navbar li ul').hide().removeClass('fallback');
+    $('#nav li').hover(
+      function() {
+        $('ul', this).stop().slideDown(100);
+  },
+  function () {
+    $('ul', this).stop().slideUp(100);
+  }
+);
