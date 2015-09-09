171790a06d8c496a14a5375a85d0ed940518b5d2
whicheverpeady <whicheverpeady@gmail.com>
2015-09-08 21:13:08 -0400

    Add user_sign_up spec

--- spec/requests/user_sign_up_spec.rb
+++ spec/requests/user_sign_up_spec.rb
@@ -2,11 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "UserSignUp", type: :request do
 
+
+  before(:each) do
+    @user = build(:user)
+  end
+
   describe "user signing up" do
     
     it "is a successful sign up" do
-      visit '/users/sign_up' 
-      expect(page).to have_selector "h2", text: "Sign up"
+      visit "/users/sign_up" 
+      expect(page).to have_selector("h2", text: "Sign up")
+      expect { post "/users", params: { email: @user.email, password: @user.password, password_confirmation: @user.password_confirmation }.to change(User.count).by 1 }
     end
 
   end
