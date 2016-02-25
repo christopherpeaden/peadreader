80bb00e29c18d7af0f8a1a9bbdac517557ec084d
whicheverpeady <whicheverpeady@gmail.com>
2016-02-25 02:46:12 -0500

    Change published to published_at on items table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160224193653) do
+ActiveRecord::Schema.define(version: 20160225074142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20160224193653) do
   create_table "items", force: :cascade do |t|
     t.string   "title"
     t.string   "url"
-    t.datetime "published"
+    t.datetime "published_at"
     t.integer  "feed_id"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
