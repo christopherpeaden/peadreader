927848fe3f080749c974617b2ee897b9e222c5a2
whicheverpeady <whicheverpeady@gmail.com>
2016-05-03 17:11:57 -0400

    Browser window scroll to top after clicking ajax link

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -1,6 +1,7 @@
 $(function() {
   $("div.container").on("click", ".pagination a", function() {
     $.get(this.href, null, null, "script")
+    $(window).scrollTop(0);
     return false;
   });
 });
