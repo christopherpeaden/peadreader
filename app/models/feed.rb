6000819d0885f678447fd83de3282c589b3f1eb6
whicheverpeady <whicheverpeady@gmail.com>
2016-05-16 01:03:31 -0400

    Install cocoon and add bin to source control

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -3,6 +3,7 @@ class Feed < ActiveRecord::Base
   has_many :categorizations
   has_many :categories, through: :categorizations
   has_many :items, dependent: :destroy
+  accepts_nested_attributes_for :categories
 
   validates :title, presence: true, uniqueness: true
   validates :url,   presence: true, uniqueness: true
