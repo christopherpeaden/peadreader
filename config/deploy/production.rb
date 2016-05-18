7910b6a3a1260508b7737c1ccabc0628a246c63c
whicheverpeady <whicheverpeady@gmail.com>
2016-05-17 22:48:00 -0400

    Remove redundant requires from capistrano configuration

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,6 @@
+set :stage production
+server '104.236.2.108', user: 'deploy', roles: %w{web app}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
