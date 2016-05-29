71ba4d1c7536fcb421edcfae26be7979651a59ca
whicheverpeady <whicheverpeady@gmail.com>
2016-05-29 14:23:05 -0400

    Upgrade to rails5.0.0rc1

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_feed_reader_session'
+Rails.application.config.session_store :cookie_store, key: '_pead_reader_session'
