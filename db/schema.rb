7761a498f8cd1753832eb1233b95d20837171c19
christopherpeaden <peadenchristopher@gmail.com>
2016-07-12 11:48:19 -0400

    Reconfigure database settings

--- db/schema.rb
+++ db/schema.rb
@@ -73,12 +73,6 @@ ActiveRecord::Schema.define(version: 20160613010029) do
     t.index ["user_id"], name: "index_job_watchers_on_user_id", using: :btree
   end
 
-  create_table "jobwatchers", force: :cascade do |t|
-    t.boolean  "completed",  default: false
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                   default: "", null: false
     t.string   "encrypted_password",      default: "", null: false
@@ -100,7 +94,7 @@ ActiveRecord::Schema.define(version: 20160613010029) do
     t.string   "image"
     t.string   "access_token"
     t.string   "refresh_token"
-    t.integer  "access_token_expiration"
+    t.integer  "access_token_expiration", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
