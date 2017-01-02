f0f6d8beb95dbac6e894c1c4ce96d568c6718e34
christopherpeaden <peadenchristopher@gmail.com>
2017-01-02 18:29:51 -0500

    Remove functionality that integrates with the youtube API

--- app/controllers/youtube_channels_controller.rb
+++
@@ -1,7 +0,0 @@
-class YoutubeChannelsController < ApplicationController
-
-  def show
-    @youtube_channels = current_user.youtube_channels
-    @youtube_channel = YoutubeChannel.find(params[:id])
-  end
-end
