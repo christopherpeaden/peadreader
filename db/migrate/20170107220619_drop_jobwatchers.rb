7f9e54b11fa65247398ad0f3ee13356234c12bd1
christopherpeaden <peadenchristopher@gmail.com>
2017-05-27 14:33:11 -0400

    Remove orphaned jobwatcher migration

--- db/migrate/20170107220619_drop_jobwatchers.rb
+++
@@ -1,9 +0,0 @@
-class DropJobwatchers < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :jobwatchers do |t|
-      t.boolean :completed, null: false, default: false
-
-      t.timestamps null: false
-    end
-  end
-end
